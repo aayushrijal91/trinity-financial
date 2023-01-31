94c1110c8c3e78583ae0dd0b328d789f02371af1
Aayush Rijal <arijal@aiims.com.au>
2023-01-31 12:16:09 +1100

    about page done

--- src/theme-files/parts/section-assisted_services.php
+++ src/theme-files/parts/section-assisted_services.php
@@ -1,5 +1,35 @@
 <div class="assisted_services">
     <div class="container">
-        <div class="ls-2 fw-600 fs-50 text-center lh-1_6">Our team will assist you in researching, organising, & negotiating loans on your behalf. <span class="fw-900">View our services below.</span></div>
+        <div class="fw-600 fs-50 text-center lh-1_6"><?= get_field('assisted_services', 'options')['heading'] ?></div>
+        <div class="row gy-4 gx-xl-6 pt-6 pt-lg-9">
+            <?php
+            if (have_rows('assisted_services', 'options')) :
+                while (have_rows('assisted_services', 'options')) : the_row();
+                    if (have_rows('assisted_services_list', 'options')) :
+                        while (have_rows('assisted_services_list', 'options')) : the_row();
+            ?>
+                            <div class="col-lg-6">
+                                <div class="assisted_service_card">
+                                    <div class="row align-items-center">
+                                        <div class="col">
+                                            <div class="fs-24"><?= the_sub_field('title') ?></div>
+                                            <div class="fs-13"><?= the_sub_field('sub_title') ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="22.6906" cy="22.6237" r="20.484" stroke="#353F94" stroke-width="3" />
+                                                <path d="M21.1045 28.4394V15.9564H23.5882V28.4394H21.1045ZM16.1095 23.4351V20.9514H28.5924V23.4351H16.1095Z" fill="white" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            <?php
+                        endwhile;
+                    endif;
+                endwhile;
+            endif;
+            ?>
+        </div>
     </div>
 </div>
--- src/theme-files/parts/section-bannerHome.php
+++ src/theme-files/parts/section-bannerHome.php
@@ -8,7 +8,7 @@
                     <h1 class="ls-3 fs-75 fw-600 text-center"><?= get_field('banner')['heading']; ?></h1>
                     <div class="text-center text-grey ls-3 fs-25 py-6"><?= get_field('banner')['sub_heading']; ?></div>
                     <div class="text-center">
-                        <a href="" class="btn btn-black fs-21 rounded-0 py-4 px-6 fw-600 ls-2">Get Started</a>
+                        <a href="" class="btn btn-black fs-21 rounded-0 py-4 px-6 fw-600">Get Started</a>
                     </div>
                 </div>
             </div>
