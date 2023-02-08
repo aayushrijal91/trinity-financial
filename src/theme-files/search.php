ce8822418390ed55fc684595e2816f721748186c
Aayush Rijal <arijal@aiims.com.au>
2023-02-08 15:05:00 +1100

    Search results page done

--- src/theme-files/search.php
+++ src/theme-files/search.php
@@ -1,12 +1,29 @@
 <?php
 get_header();
-get_template_part('parts/section', 'banner');
+get_template_part('parts/section', 'nav');
 ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php the_title(); ?>
-        <?php the_content(); ?>
-    <?php endwhile;
-else : ?>
-    <p><?php esc_html_e('Sorry, no post matched your criteria.') ?></p>
-<?php endif; ?>
+<header class="subpage_header">
+        <div class="container">
+            <div class="ls-3 fs-75 fw-600 text-primary">Search Results...</div>
+        </div>
+    </header>
+<section class="search_result">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10 col-xl-9">
+                        <div class="py-4">
+                            <a href="<?= get_the_permalink() ?>" class="fs-24 fw-600"><?= get_the_title() ?></a>
+                            <div class="fw-500 pt-3 lh-1_87 fs-18"><?= get_the_content(); ?></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile;
+    else : ?>
+        <div class="container">
+            <p><?php esc_html_e('Sorry, no post matched your criteria.') ?></p>
+        </div>
+    <?php endif; ?>
+</section>
 <?php get_footer(); ?>
