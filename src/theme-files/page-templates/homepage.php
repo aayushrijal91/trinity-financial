faf148aa7b9f8b4e2797501ee5a55bbe5afd761b
Aayush Rijal <arijal@aiims.com.au>
2023-02-08 16:59:56 +1100

    WIP: mobile theme

--- src/theme-files/page-templates/homepage.php
+++ src/theme-files/page-templates/homepage.php
@@ -13,16 +13,16 @@ get_template_part('parts/section', 'bannerHome');
         <?php $section_1 = get_field('section_1'); ?>
         <div class="container">
             <div class="row gx-4">
-                <div class="col-7">
-                    <div class="fs-75 lh-1_2"><?= $section_1['heading'] ?></div>
-                    <div class="row justify-content-end mt-9">
-                        <div class="col-8">
-                            <div class="fs-37"><?= $section_1['sub_heading'] ?></div>
-                            <div class="text-center pt-7"><a href="<?= $section_1['button']['url'] ?>" class="btn btn-primary fs-21 rounded-0 py-4 px-5 fw-600"><?= $section_1['button']['title'] ?></a></div>
+                <div class="col-md-7">
+                    <div class="fs-75 lh-1_2 heading"><?= $section_1['heading'] ?></div>
+                    <div class="row justify-content-md-end mt-5 mt-md-9">
+                        <div class="col-md-8">
+                            <div class="fs-37 sub_heading"><?= $section_1['sub_heading'] ?></div>
+                            <div class="text-center pt-5 pt-md-7"><a href="<?= $section_1['button']['url'] ?>" class="btn btn-primary fs-21 rounded-0 py-3 py-md-4 px-5 fw-600"><?= $section_1['button']['title'] ?></a></div>
                         </div>
                     </div>
                 </div>
-                <div class="col-5">
+                <div class="col-md-5">
                     <div class="contact_us_form">
                         <?= do_shortcode('[contact-form-7 id="133" title="Contact Us"]') ?>
                     </div>
@@ -37,7 +37,7 @@ get_template_part('parts/section', 'bannerHome');
             <div class="row">
                 <div class="col-md-6">
                     <div class="fs-70 fw-400 ls-2 lh-1"><?= $section_2['heading'] ?></div>
-                    <div class="fs-23 lh-1_87 py-6 description"><?= $section_2['description'] ?></div>
+                    <div class="fs-23 lh-1_87 py-4 py-md-6 description"><?= $section_2['description'] ?></div>
                     <a href="<?= $section_2['button']['url'] ?>" class="btn btn-primary fs-21 rounded-0 py-4 px-5 fw-600"><?= $section_2['button']['title'] ?></a>
                 </div>
                 <div class="col-md-6">
