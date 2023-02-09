5d95e8705077966e6994719adcbf97b9a48ca6d8
Aayush Rijal <arijal@aiims.com.au>
2023-02-09 14:29:15 +1100

    Service template 5 responsive done

--- src/theme-files/single-services.php
+++ src/theme-files/single-services.php
@@ -9,8 +9,8 @@ $template = get_field('choose_template');
         <div class="header">
             <div class="container">
                 <div class="ls-2 fw-900 fs-60"><?= $header['title'] ?></div>
-                <div class="fs-21 pt-6 pb-7 description"><?= $header['description'] ?></div>
-                <a href="<?= $header['button']['url'] ?>" class="btn btn-primary fs-21 rounded-0 py-4 px-6 fw-600"><?= $header['button']['title'] ?></a>
+                <div class="fs-21 py-4 py-md-5 pt-lg-6 pb-lg-7 description"><?= $header['description'] ?></div>
+                <a href="<?= $header['button']['url'] ?>" class="btn btn-primary fs-21 rounded-0 py-3 py-md-4 px-5 px-md-6 fw-600"><?= $header['button']['title'] ?></a>
             </div>
         </div>
     <?php endif; ?>
@@ -209,7 +209,7 @@ $template = get_field('choose_template');
             <?php if (!empty($section_1['title'])) : ?>
                 <div class="section_1">
                     <div class="container">
-                        <div class="text-center fs-50 fw-600 ls-2 mb-8"><?= $section_1['title'] ?></div>
+                        <div class="text-center fs-50 fw-600 ls-2 mb-5 mg-lg-6 mb-xl-8"><?= $section_1['title'] ?></div>
                         <?php
                         $totalList = count($section_1['section_1_list']);
 
@@ -221,12 +221,14 @@ $template = get_field('choose_template');
                                             $index = 1;
                                             while (have_rows('section_1_list')) : the_row();
                         ?>
-                                                <div class="py-4 fs-24 fw-500 ls-2 d-flex align-items-center">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
-                                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
-                                                    </svg>
-                                                    <div class="ps-4"><?= get_sub_field('text') ?></div>
+                                                <div class="py-3 py-md-4 fs-24 fw-500 ls-2 d-flex align-items-center list">
+                                                    <div>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div class="ps-3 ps-md-4"><?= get_sub_field('text') ?></div>
                                                 </div>
                                                 <?php if ($index < $totalList) : ?>
                                                     <hr>
@@ -247,12 +249,12 @@ $template = get_field('choose_template');
             <?php if (!empty($section_2['title'])) : ?>
                 <div class="section_2">
                     <div class="container">
-                        <div class="row align-items-center gx-lg-6">
-                            <div class="col-6">
-                                <div class="fs-60 fw-500 pb-6"><?= $section_2['title'] ?></div>
+                        <div class="row justify-content-center align-items-center gx-lg-6 gy-5">
+                            <div class="col-xl-6">
+                                <div class="fs-60 fw-500 pb-5 pb-md-6"><?= $section_2['title'] ?></div>
                                 <div class="description fs-23 lh-2"><?= $section_2['description'] ?></div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-lg-auto col-xl-6">
                                 <img src="<?= $section_2['image']['url'] ?>" alt="<?= $section_2['image']['alt'] ?>">
                             </div>
                         </div>
@@ -265,7 +267,7 @@ $template = get_field('choose_template');
     <?php $footer = get_field('footer'); ?>
     <?php if (!empty($footer['image']['url'])) : ?>
         <div class="footer">
-            <img src="<?= $footer['image']['url'] ?>" alt="frames" class="w-100">
+            <img src="<?= $footer['image']['url'] ?>" alt="frames" class="main_img w-100">
             <div class="inner">
                 <div class="container-fluid">
                     <div class="row justify-content-end">
