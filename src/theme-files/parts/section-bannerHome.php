f32b2f7d4f0e97181577c6db788897c2f7fa36d3
Aayush Rijal <arijal@aiims.com.au>
2023-02-13 11:07:53 +1100

    Ready for staging

--- src/theme-files/parts/section-bannerHome.php
+++ src/theme-files/parts/section-bannerHome.php
@@ -8,7 +8,7 @@
                     <h1 class="ls-3 fs-75 fw-600 text-center"><?= get_field('banner')['heading']; ?></h1>
                     <div class="text-center text-grey ls-3 fs-25 py-5 py-lg-6"><?= get_field('banner')['sub_heading']; ?></div>
                     <div class="text-center">
-                        <a href="" class="btn btn-black fs-21 rounded-0 py-3 py-lg-4 px-4 px-md-5 px-lg-6 fw-600">Get Started</a>
+                        <a href="<?= get_field('banner')['button']['url']; ?>" class="btn btn-black fs-21 rounded-0 py-3 py-lg-4 px-4 px-md-5 px-lg-6 fw-600"><?= get_field('banner')['button']['title']; ?></a>
                     </div>
                 </div>
             </div>
